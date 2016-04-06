6fa9a00f05c810a682548fbc29b99a5fa5f4868a
Andre <andrethegamer911@gmail.com>
2016-04-05 20:02:15 -0400

    Saved

---
+++ config.php
@@ -0,0 +1,12 @@
+#Configuration file
+
+Site-Title: Test 
+Backgroundu-url: 
+
+
+// Make sure to follow the formats
+services:
+   name1: Codding
+     description: i love codding lol cause i love codding
+   name2: Website designer
+     description: i've been making site lol  for  fun
